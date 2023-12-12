d15d3edb7623ba2b918740109d2ef135be476931
Jonathan Bonetti <j.bonetti@sintechsolution.it>
2023-12-12 17:54:11 +0100

    upd

--- day11/round1.py
+++ day11/round1.py
@@ -1,10 +1,27 @@
+import numpy as np
+
 file = open('text.txt','r')
 
-def expander(universo_matrix):
+def aggiungi_righe_universo(universo_matrix):
+    universo_espanso_verticale = []
     for i, riga in enumerate(universo_matrix):
-        if '#' not in riga:
-            universo_matrix.insert(i-1, ['.' for j in range(len(riga))])
+        if '#' in riga:
+            universo_espanso_verticale.append(riga)
+        else:
+            universo_espanso_verticale.append(['.' for j in range(len(riga))])
+            universo_espanso_verticale.append(['.' for j in range(len(riga))])
+    return universo_espanso_verticale
 
+def aggiungi_colonne_universo(universo_da_espandere):
+    universo_trasposto = np.transpose(universo_da_espandere)
+    universo_espanso = aggiungi_righe_universo(universo_trasposto)
+    universo_espanso_finale = np.transpose(universo_espanso)
+    return universo_espanso_finale
+
+def expander(universo):
+    universo_espanso_h = aggiungi_righe_universo(universo)
+    universo_finale = aggiungi_colonne_universo(universo_espanso_h)
+    return universo_finale
 
 the_matrix = []
 for riga in file:
---
+++ day11/test.py
@@ -0,0 +1,5 @@
+import numpy as np 
+#a = np.array([[5,3], [ 4,2]])
+a = [[11,3], [ 4,2]]
+#a.append([11,7])
+print(np.transpose(a))
